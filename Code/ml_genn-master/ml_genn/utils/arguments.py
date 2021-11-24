648ec67aa5c3bfc8da33287c2ad51a560f598181
thomasshoesmith <thomas.shoesmith@yahoo.com>
2021-11-24 10:12:53 +0000

    updating

--- Code/ml_genn-master/ml_genn/utils/arguments.py
+++ Code/ml_genn-master/ml_genn/utils/arguments.py
@@ -49,10 +49,10 @@ def parse_arguments(model_description='ML GeNN model'):
         if self.converter == 'few-spike':
             return FewSpike(K=K, signed_input=signed_input, norm_data=[norm_data])
         elif args.converter == 'data-norm':
-            return DataNorm(norm_data=[norm_data], signed_input=signed_input, 
+            return DataNorm(norm_data=[norm_data], signed_input=signed_input,
                             input_type=self.input_type)
         elif args.converter == 'spike-norm':
-            return SpikeNorm(norm_data=[norm_data], norm_time=norm_time, 
+            return SpikeNorm(norm_data=[norm_data], norm_time=norm_time,
                              signed_input=signed_input, input_type=self.input_type)
         else:
             return Simple(signed_input=signed_input, input_type=self.input_type)
