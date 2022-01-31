ee28058d4d1446bcfc2cdc3cdd4edfe48a6924a5
thomasshoesmith <thomas.shoesmith@yahoo.com>
2022-01-31 10:57:03 +0000

    v3

---
+++ Code_2/Primary_Objective_1_2_CIFAR10/just_MNIST_2.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers, datasets
+import tensorflow_datasets as tfds
+
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+
+print(len(x_train))
+print(len(x_test))
+
+print(x_train[0])
+print(y_train[0])
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model.fit(x_train, y_train, epochs=2, validation_data=ds_test)
+
