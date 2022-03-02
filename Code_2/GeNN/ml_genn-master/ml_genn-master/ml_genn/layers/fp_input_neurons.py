3475b8786a9578a718ffebbae44595da2aa91ca9
thomasshoesmith <thomas.shoesmith@yahoo.com>
2022-03-02 21:41:18 +0000

    Update fp_input_neurons.py

--- Code_2/GeNN/ml_genn-master/ml_genn-master/ml_genn/layers/fp_input_neurons.py
+++ Code_2/GeNN/ml_genn-master/ml_genn-master/ml_genn/layers/fp_input_neurons.py
@@ -2,14 +2,17 @@ from pygenn.genn_model import create_dpf_class, create_custom_neuron_class
 from pygenn.genn_wrapper.Models import VarAccess_READ_ONLY_DUPLICATE
 from ml_genn.layers.input_neurons import InputNeurons
 
-fs_relu_input_model = create_custom_neuron_class(
-    'fs_relu_input',
-    param_names=['K', 'alpha'],
-    derived_params=[("scale", create_dpf_class(lambda pars, dt: pars[1] * 2**(-pars[0]))())],
-    var_name_types=[('input', 'scalar', VarAccess_READ_ONLY_DUPLICATE), ('Vmem', 'scalar')],
+fp_relu_input_model = create_custom_neuron_class(
+    'fp_relu_input',
+    param_names=['K', 'alpha', 'elim'], #k = timesteps, alpha = highest value, elim = length of exponent
+    derived_params=[("scale", create_dpf_class(lambda pars, dt: pars[1] * 2**(-pars[0]))())], #TODO pars[0] = K, pars[1] = Alpha
+    var_name_types=[('input', 'scalar', VarAccess_READ_ONLY_DUPLICATE), ('Vmem', 'scalar')],  
     sim_code='''
     // Convert K to integer
     const int kInt = (int)$(K);
+
+    // Convert elim to integer
+    const int elimInt = (int)$(elim);
     
     // Get timestep within presentation
     const int pipeTimestep = (int)($(t) / DT);
@@ -29,7 +32,7 @@ fs_relu_input_model = create_custom_neuron_class(
     ''',
     is_auto_refractory_required=False)
 
-fs_relu_signed_input_model = create_custom_neuron_class(
+fp_relu_signed_input_model = create_custom_neuron_class(
     'fs_relu_signed_input',
     param_names=['K', 'alpha'],
     derived_params=[("scale", create_dpf_class(lambda pars, dt: pars[1] * 2**(-pars[0]//2))())],
@@ -64,18 +67,18 @@ fs_relu_signed_input_model = create_custom_neuron_class(
     ''',
     is_auto_refractory_required=False)
 
-class FSReluInputNeurons(InputNeurons):
-    def __init__(self, K=10, alpha=25, signed_input=False):
-        super(FSReluInputNeurons, self).__init__()
+class FPReluInputNeurons(InputNeurons):
+    def __init__(self, K=10, alpha=25, elim = 4, signed_input=False):
+        super(FPReluInputNeurons, self).__init__()
         self.K = K
         self.alpha = alpha
+        self.elim = elim
         self.signed_input = signed_input
 
     def compile(self, mlg_model, layer):
-        model = (fs_relu_signed_input_model if self.signed_input 
-                 else fs_relu_input_model)
-        params = {'K' : self.K, 'alpha': self.alpha}
+        model = fp_relu_input_model
+        params = {'K' : self.K, 'alpha': self.alpha, 'elim' : self.elim}
         vars = {'input': 0.0, 'Vmem': 0.0}
 
-        super(FSReluInputNeurons, self).compile(mlg_model, layer, model,
+        super(FPReluInputNeurons, self).compile(mlg_model, layer, model,
                                                 params, vars, {})
