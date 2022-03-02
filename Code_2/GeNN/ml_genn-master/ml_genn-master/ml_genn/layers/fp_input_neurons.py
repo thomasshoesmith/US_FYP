6f554c3ed8ea1005c46ca3d9b3b7b309fe4def64
thomasshoesmith <thomas.shoesmith@yahoo.com>
2022-03-02 23:11:02 +0000

    Update fp_input_neurons.py

--- Code_2/GeNN/ml_genn-master/ml_genn-master/ml_genn/layers/fp_input_neurons.py
+++ Code_2/GeNN/ml_genn-master/ml_genn-master/ml_genn/layers/fp_input_neurons.py
@@ -6,14 +6,14 @@ fp_relu_input_model = create_custom_neuron_class(
     'fp_relu_input',
     param_names=['K', 'alpha', 'elim'], #k = timesteps, alpha = highest value, elim = length of exponent
     derived_params=[("scale", create_dpf_class(lambda pars, dt: pars[1] * 2**(-pars[0]))())], #TODO pars[0] = K, pars[1] = Alpha
-    var_name_types=[('input', 'scalar', VarAccess_READ_ONLY_DUPLICATE), ('Vmem', 'scalar')],  
+    var_name_types=[('input', 'scalar', VarAccess_READ_ONLY_DUPLICATE), ('Vmem', 'scalar')],
     sim_code='''
     // Convert K to integer
     const int kInt = (int)$(K);
 
     // Convert elim to integer
     const int elimInt = (int)$(elim);
-    
+
     // Get timestep within presentation
     const int pipeTimestep = (int)($(t) / DT);
 
@@ -21,7 +21,7 @@ fp_relu_input_model = create_custom_neuron_class(
     if(pipeTimestep == 0) {
         $(Vmem) = $(input);
     }
-    
+
     const scalar hT = $(scale) * (1 << (kInt - (1 + pipeTimestep)));
     ''',
     threshold_condition_code='''
@@ -29,41 +29,6 @@ fp_relu_input_model = create_custom_neuron_class(
     ''',
     reset_code='''
     $(Vmem) -= hT;
-    ''',
-    is_auto_refractory_required=False)
-
-fp_relu_signed_input_model = create_custom_neuron_class(
-    'fs_relu_signed_input',
-    param_names=['K', 'alpha'],
-    derived_params=[("scale", create_dpf_class(lambda pars, dt: pars[1] * 2**(-pars[0]//2))())],
-    var_name_types=[('input', 'scalar', VarAccess_READ_ONLY_DUPLICATE), ('Vmem', 'scalar')],
-    sim_code='''
-    // Convert K to integer
-    const int halfK = (int)$(K) / 2;
-
-    // Get timestep within presentation
-    const int pipeTimestep = (int)($(t) / DT);
-
-    // If this is the first timestep, apply input
-    if(pipeTimestep == 0) {
-        $(Vmem) = $(input);
-    }
-
-    // Split timestep into interleaved positive and negative
-    const int halfPipetimestep = pipeTimestep / 2;
-    const bool positive = (pipeTimestep % 2) == 0;
-    const scalar hT = $(scale) * (1 << (halfK - (1 + halfPipetimestep)));
-    ''',
-    threshold_condition_code='''
-    (positive && $(Vmem) >= hT) || (!positive && $(Vmem) < -hT)
-    ''',
-    reset_code='''
-    if(positive) {
-        $(Vmem) -= hT;
-    }
-    else {
-        $(Vmem) += hT;
-    }
     ''',
     is_auto_refractory_required=False)
 
